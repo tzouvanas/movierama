8c0e272d28302f8718849c0b12eb523b571eddac
tzouvanas <ktzuvan.personal@gmail.com>
2020-10-11 14:20:55 +0300

    RFC : Move code out of cshtml

---
+++ movierama.server/Views/Home/ReviewActionHiddenAttributeExtension.cs
@@ -0,0 +1,39 @@
+﻿using Movierama.Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movierama.Server.Views.Home
+{
+    public static class ReviewActionHiddenAttributeExtension
+    {
+        public static string HiddenAttributeFor(string element, ReviewOpinion opinion) 
+        {
+            bool shouldBeHidden = false;
+
+            switch (element)
+            {
+                case "Hate":
+                case "Like":
+                    shouldBeHidden = opinion != ReviewOpinion.Neutral;
+                    break;
+
+                case "Unhate":
+                case "Hate_Description":
+                    shouldBeHidden = opinion != ReviewOpinion.Hate;
+                    break;
+
+                case "Unlike":
+                case "Like_Description":
+                    shouldBeHidden = opinion != ReviewOpinion.Like;
+                    break;
+            }
+
+            if (shouldBeHidden)
+                return "hidden";
+
+            return string.Empty;
+        }
+    }
+}
